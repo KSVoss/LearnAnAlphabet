d73160928762046585df0ce496201171ebf76f03
KSVoss <Kai.S.Voss@t-online.de>
2022-08-24 09:17:02 +0200

    FixSonarLintIssues3rd

--- backend/src/main/java/ksvoss/backend/models/Letter.java
+++ backend/src/main/java/ksvoss/backend/models/Letter.java
@@ -1,5 +1,8 @@
 package ksvoss.backend.models;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public record Letter(
         int id,
         String signAsText,
@@ -9,4 +12,31 @@ public record Letter(
         int pronunciationDurationMs,
         String pronunciationUrl
         ) {
+        @Override
+        public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+                Letter letter = (Letter) o;
+                return id == letter.id && pronunciationStartInMs == letter.pronunciationStartInMs && pronunciationDurationMs == letter.pronunciationDurationMs && Objects.equals(signAsText, letter.signAsText) && Arrays.equals(signAsPicture, letter.signAsPicture) && Objects.equals(spelling, letter.spelling) && Objects.equals(pronunciationUrl, letter.pronunciationUrl);
+        }
+
+        @Override
+        public int hashCode() {
+                int result = Objects.hash(id, signAsText, spelling, pronunciationStartInMs, pronunciationDurationMs, pronunciationUrl);
+                result = 31 * result + Arrays.hashCode(signAsPicture);
+                return result;
+        }
+
+        @Override
+        public String toString() {
+                return "Letter{" +
+                        "id=" + id +
+                        ", signAsText='" + signAsText + '\'' +
+                        ", signAsPicture=" + Arrays.toString(signAsPicture) +
+                        ", spelling='" + spelling + '\'' +
+                        ", pronunciationStartInMs=" + pronunciationStartInMs +
+                        ", pronunciationDurationMs=" + pronunciationDurationMs +
+                        ", pronunciationUrl='" + pronunciationUrl + '\'' +
+                        '}';
+        }
 }
