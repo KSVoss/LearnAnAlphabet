e9e1f0490dae22c82043c9d5985cee0400bd53dd
KSVoss <Kai.S.Voss@t-online.de>
2022-08-24 08:49:21 +0200

    FixSonarLintIssues

--- backend/src/main/java/ksvoss/backend/models/Alphabet.java
+++ backend/src/main/java/ksvoss/backend/models/Alphabet.java
@@ -1,5 +1,8 @@
 package ksvoss.backend.models;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public record Alphabet(
         int id,
 	    AlphabetNameInDifferentLanguage[] names,
@@ -7,4 +10,26 @@ public record Alphabet(
         Letter[] letters
 
 ) {
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Alphabet alphabet = (Alphabet) o;
+		return id == alphabet.id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public String toString() {
+		return "Alphabet{" +
+				"id=" + id +
+				", names=" + Arrays.toString(names) +
+				", pronunciationSoundfile=" + Arrays.toString(pronunciationSoundfile) +
+				", letters=" + Arrays.toString(letters) +
+				'}';
+	}
 }
